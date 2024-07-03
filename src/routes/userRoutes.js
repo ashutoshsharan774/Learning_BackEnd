7c2d78f5ca7849d2143216daf3a7080ef2c85077
ASHUTOSH_m1 <ashutosh_m1@ASHUTOSH-m1s-MacBook-Air.local>
2024-07-03 12:59:10 +0530

    add new routes/endpoints for usercontrollers

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { logOutUser, loginUser, refreshaccesstoken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentUserPassword,
+     getCurrentUser,
+     getUserChannelProfile,
+     getWatchHistory,
+     logOutUser,
+     loginUser,
+     refreshaccesstoken,
+     registerUser,
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage } from "../controllers/user.controller.js";
 //is tarah ka import tabhi ho skta ha jb export default na ho
 
 import {upload} from "../middlewares/Multer.middleware.js"
@@ -33,6 +43,35 @@ router.route("/logout").post( verifyJwt, logOutUser)
 
 //endpoint for refreshtoken
 router.route("/refresh-token").post(refreshaccesstoken)
+
+//endpoint of changePassword:change-password is our url
+router.route("/change-Password").post( verifyJwt,changeCurrentUserPassword)//ensure with help of verifyjwt ki logged in users hi changeCurrentPassword method ko post kr paaye
+
+//endpoint of currentuser
+router.route("/current-user").get(verifyJwt,getCurrentUser)
+
+//endpoint for updating account details yaha patch route lagana h
+//The PATCH method in HTTP is used to apply partial modifications to a resource. Unlike the PUT method, which typically updates the entire resource, PATCH is designed to update specific parts of a resource. This makes it more efficient for certain updates, as it allows clients to send only the changes rather than the complete resource.
+router.route("/update-account-details").patch(verifyJwt,updateAccountDetails)
+
+//endpoint for updation of useravatar
+router
+.route("/update-user-avatar")
+.patch(verifyJwt,upload.single("avatar"),updateUserAvatar) //verifyJwt is our first middleware, 2nd middleware se upload(multer) krenge pahle new avatar ko then method call hoga
+
+//endpoint for updation of user's coverImage //same as avatar
+router.route("/update-coverImg").patch(verifyJwt,upload.single("coverImage"),updateUserCoverImage)
+
+//endpoint for channel profile-> we are taking from params
+/*
+In the context of backend development, "params" (short for parameters) are values passed to an API endpoint to modify the request. They allow the client to specify additional information, which can be used to tailor the server's response. There are three main types of parameters: path parameters, query parameters, and body parameters. */
+router.route("/channel/:username").get(verifyJwt,getUserChannelProfile)
+
+//endpoint for watchHistory of user
+router.route("/history").get(verifyJwt,getWatchHistory)
+
+
+
 
 
 export default router
